a78f43b6708a5d9ca5f1b199f35d6bfdd0226c69
Pedro Lucas Grajaú <84031272+Pedro-Grajau@users.noreply.github.com>
2021-05-22 11:43:00 -0300

    Challenge Complete

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,44 @@
-class Rectangle:
+class Rectangle():
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
 
+  def __str__(self):
+    return f'Rectangle(width={self.width}, height={self.height})'
 
+  def get_area(self):
+    return self.height * self.width
 
+  def get_perimeter(self):
+    return 2 * self.height + 2 * self.width
 
-class Square:
+  def get_picture(self):
+    shape = ""
+    if self.height > 50 or self.width > 50:
+      return "Too big for picture."
+    for i in range(self.height):
+      shape += "*" * self.width + "\n"
+    return shape
+
+  def get_diagonal(self):
+    return ((self.width ** 2 + self.height ** 2) ** .5)
+
+  def set_height(self, height):
+    self.height = height
+
+  def set_width(self, width):
+    self.width = width
+
+  def get_amount_inside(self, shape):
+    return self.get_area() // shape.get_area()
+    
+class Square(Rectangle):
+  def __init__(self, width):
+    super().__init__(width, width)
+
+  def __str__(self):
+    return f'Square(side={self.width})'
+  
+  def set_side(self, side):
+    self.width = side
+    self.height = side
